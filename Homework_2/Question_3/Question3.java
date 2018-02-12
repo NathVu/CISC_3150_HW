432d05e3f5df053d25a83bbde855b346e3dfa351
Nath_Vu <Nath_Vu@github.com>
2018-02-12 18:52:09 -0500

    Question3
    Q3 code and output completed and pushed

---
+++ Homework_2/Question_3/Question3.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Question3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //String pal = input.next();
+        while(input.hasNext()) {
+            String pal = input.next();
+            boolean isPal = true;
+            for(int i = 0; i < (pal.length()/2); i++){
+                if(pal.charAt(i) != pal.charAt(pal.length() - i - 1)){
+                    isPal = false;
+                    break;
+                }
+            }
+            if(isPal == true) {
+                System.out.println(pal + " is a palindrome");
+            }
+            else {
+                System.out.println(pal + " is not a palindrome");
+            }
+        }
+    }
+}
